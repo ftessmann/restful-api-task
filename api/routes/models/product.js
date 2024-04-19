21722deb6793fd2cb142438e46aae0ad34c35cea
ftessmann <fernando.tessmann@gmail.com>
2024-04-19 16:58:05 -0300

    add required and image property

--- api/routes/models/product.js
+++ api/routes/models/product.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose");
 
 const productSchema = mongoose.Schema({
-    _id: mongoose.Types.ObjectId,
-    name: String,
-    price: Number,
-    category: String
+    _id: mongoose.Schema.Types.ObjectId,
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+    category: String,
+    productImage: String
 });
 
 module.exports = mongoose.model("Product", productSchema);
