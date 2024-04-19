d85f58c4a341424a52e5924f9029fbc74a25e971
ftessmann <fernando.tessmann@gmail.com>
2024-04-19 16:59:07 -0300

    orders routes handler

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,38 +1,123 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+
+const Order = require("./models/order");
+const Product = require("./models/product");
 
 // handle router for /orders
 router.get("/", (req, res, next) => {
-    res.status(200).json({
-        message: "GET order"
+    Order.find()
+         .select("product quantity _id vendor")
+         .populate("product", "name")
+         .exec()
+         .then(docs => {
+            res.status(200).json({
+                count: docs.lenght,
+                orders: docs.map(doc => {
+                    return {
+                        _id: doc._id,
+                        product: doc.product,
+                        quantity: doc.quantity,
+                        vendor: doc.vendor,
+                        request: {
+                            type: "GET",
+                            url: "http://localhost:3000/orders/" + doc._id
+                        }
+                    }
+                }),
+            });
+         })
+         .catch(err => {
+            res.status(500).json({
+                error: err
+            })
+         })
     });
-});
 
 router.post("/", (req, res, next) => {
-    const order = {
-        productId: req.body.productId,
-        quantity: req.body.quantity,
-        vendor: req.body.vendor
-    }
-    res.status(201).json({
-        message: "POST order",
-        createdOrder: order
-    });
+    Product.findById(req.body.productId)
+           .then(product => {
+            if (!product) {
+                return res.status(404).json({
+                    message: "Product not found"
+                });
+            }
+            const order = new Order({
+                _id: new mongoose.Types.ObjectId(),
+                quantity: req.body.quantity,
+                product: req.body.productId,
+                vendor: req.body.vendor
+            });
+            return order.save()
+        })
+
+        .then(result => {
+            console.log(result);
+            res.status(201).json({
+                message: "Order created",
+                createdOrder: {
+                    _id: result._id,
+                    product: result.product,
+                    quantity: result.quantity,
+                    vendor: result.vendor
+                },
+                request: {
+                    type: "GET",
+                    url: "http://localhost:3000/orders/" + result._id
+                }
+            });
+         })
+         .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            })
+         })
 });
+    
 
 // handle router for /orders/:id
 router.get("/:orderId", (req, res, next) => {
-    res.status(200).json({
-        message: "GET orderId",
-        orderId: req.params.orderId
-    });
+    Order.findById(req.params.orderId)
+        .populate("product")
+        .exec()
+        .then(order => {
+            res.status(200).json({
+                order: order,
+                request: {
+                    type: "GET",
+                    url: "http://localhost:3000/orders"
+                }
+            })
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            })
+        })
 });
 
 router.delete("/:orderId", (req, res, next) => {
-    res.status(200).json({
-        message: "DELETE order",
-        orderId: req.params.orderId
-    });
+    Order.deleteOne({ _id: req.params.orderId })
+        .exec()
+        .then(result => {
+            res.status(200).json({
+                message: "Order Deleted",
+                request: {
+                    type: "POST",
+                    url: "http://localhost:3000/orders",
+                    body: { productId: "ID", quantity: "Number" }
+                }
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            })
+        })
 });
 
 
